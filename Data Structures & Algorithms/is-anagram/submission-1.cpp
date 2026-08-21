9b7dd8656bf15d72e932a22fce68c7b03907176b
KrystalAir <paul89090716@gmail.com>
2026-08-21 12:29:46 +0800

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size())
+            return false;
+        unordered_map<char, int> appear_s;
+        unordered_map<char, int> appear_t;
+        for (int i = 0; i < s.size(); i++)
+            appear_s[s[i]]++;
+        for (int i = 0; i < t.size(); i++)
+            appear_t[t[i]]++;
+        if (appear_s == appear_t)
+            return true;
+        return false;
+    }
+};
