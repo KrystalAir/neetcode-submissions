26fae5d58f290730ba4d1f9211a9fa67b6f5035b
KrystalAir <paul89090716@gmail.com>
2026-08-22 17:31:56 +0800

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution{
+public:
+    static bool cmp(pair<int, int> a, pair<int, int> b){
+        return a.second > b.second;
+    }
+    vector<int> topKFrequent(vector<int> &nums, int k){
+        unordered_map<int, int> freq;
+
+        for (int nums : nums)
+            freq[nums]++;
+
+        vector<pair<int,int>> bucket;
+
+        for (auto &it : freq)
+            bucket.emplace_back(it.first, it.second);
+
+        sort (bucket.begin(), bucket.end(), cmp);
+
+        vector<int> res;
+        
+        for (int i = 0; i < k; i++)
+            res.emplace_back(bucket[i].first);
+        return res;
+    }
+};
