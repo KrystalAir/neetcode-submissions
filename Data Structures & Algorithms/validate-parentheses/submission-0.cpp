e03ac9b5ad734952293404d838feeda418138945
crystal_air <paul89090716@gmail.com>
2026-08-24 22:29:40 +0800

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        vector<char> check;
+        for (int i = 0; i < s.size(); i++){
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{'){
+                check.emplace_back(s[i]);
+            }
+            else {
+                if (check.empty())
+                    return false;
+                if (s[i] == ')' && check.back() != '(')
+                    return false;
+                else if (s[i] == ']' && check.back() != '[')
+                    return false;
+                else if (s[i] == '}' && check.back() != '{')
+                    return false;
+                else
+                    check.pop_back();
+            }
+        }
+        if (!check.empty())
+            return false;
+        return true;
+    }
+};
